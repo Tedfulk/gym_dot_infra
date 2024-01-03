c034b90272f3cb6f7301a8c7d695686deadae001
Ted Fulk <tfulk@getspiffy.com>
2024-01-03 14:45:53 -0500

    refactor: Removed unused code

--- app/main.py
+++ app/main.py
@@ -1,259 +1,44 @@
-from typing import List, Optional
 from contextlib import asynccontextmanager
+from typing import List, Optional
+
 import httpx
 from fastapi import Depends, FastAPI, HTTPException, Query, Request
-from pydantic import constr, field_validator
-from datetime import datetime
-from enum import StrEnum
-import dns.resolver
+from sqlmodel import Session, select
 
-from sqlmodel import Session, select, SQLModel, create_engine, Field, Relationship
-
-
-states = {
-    "AK": "ALASKA",
-    "AL": "ALABAMA",
-    "AR": "ARKANSAS",
-    "AZ": "ARIZONA",
-    "CA": "CALIFORNIA",
-    "CO": "COLORADO",
-    "CT": "CONNECTICUT",
-    "DC": "DISTRICT OF COLUMBIA",
-    "DE": "DELAWARE",
-    "FL": "FLORIDA",
-    "GA": "GEORGIA",
-    "HI": "HAWAII",
-    "IA": "IOWA",
-    "ID": "IDAHO",
-    "IL": "ILLINOIS",
-    "IN": "INDIANA",
-    "KS": "KANSAS",
-    "KY": "KENTUCKY",
-    "LA": "LOUISIANA",
-    "MA": "MASSACHUSETTS",
-    "MD": "MARYLAND",
-    "ME": "MAINE",
-    "MI": "MICHIGAN",
-    "MN": "MINNESOTA",
-    "MO": "MISSOURI",
-    "MS": "MISSISSIPPI",
-    "MT": "MONTANA",
-    "NC": "NORTH CAROLINA",
-    "ND": "NORTH DAKOTA",
-    "NE": "NEBRASKA",
-    "NH": "NEW HAMPSHIRE",
-    "NJ": "NEW JERSEY",
-    "NM": "NEW MEXICO",
-    "NV": "NEVADA",
-    "NY": "NEW YORK",
-    "OH": "OHIO",
-    "OK": "OKLAHOMA",
-    "OR": "OREGON",
-    "PA": "PENNSYLVANIA",
-    "RI": "RHODE ISLAND",
-    "SC": "SOUTH CAROLINA",
-    "SD": "SOUTH DAKOTA",
-    "TN": "TENNESSEE",
-    "TX": "TEXAS",
-    "UT": "UTAH",
-    "VA": "VIRGINIA",
-    "VT": "VERMONT",
-    "WA": "WASHINGTON",
-    "WI": "WISCONSIN",
-    "WV": "WEST VIRGINIA",
-    "WY": "WYOMING",
-}
-
-
-class Address(SQLModel):
-    street: str
-    city: str
-    state: str
-    state_abbr: Optional[str]
-    zip_code: str
-
-    @field_validator("zip_code")
-    def must_be_valid_zip_code(cls, v):
-        if not v.isnumeric() or len(v) != 5:
-            raise ValueError("Zip code must be a 5-digit number")
-        return v
-
-    @field_validator("state")
-    def must_be_valid_state(cls, v):
-        if v not in cls.states.values():
-            raise ValueError("Invalid state")
-        return v
-
-    @field_validator("state_abbr")
-    def must_be_valid_state_abbr(cls, v):
-        if v not in cls.states.keys():
-            raise ValueError("Invalid state abbreviation")
-        return v
-
-    @field_validator("state_abbr")
-    def set_state_abbr(cls, v, values):
-        if "state" in values:
-            return cls.states[values["state"]]
-        return v
-
-
-class Role(StrEnum):
-    OWNER = "Owner"
-    MANAGER = "Manager"
-    TRAINER = "Trainer"
-    STAFF = "Staff"
-    MEMBER = "Member"
-    GUEST = "Guest"
-    VENDOR = "Vendor"
-
-
-class OwnerBase(SQLModel):
-    name: constr(min_length=3, max_length=100)
-    email: str
-    role: Optional[Role] = None
-    created_at: Optional[datetime] = Field(default_factory=datetime.now)
-
-    @field_validator("name")
-    def validate_name(cls, v):
-        if not v.replace(" ", "").isalpha():
-            raise ValueError("Name must only contain alphabetic characters")
-        return v.title()
-
-    @field_validator("email")
-    def validate_email(cls, v):
-        # Verify's that the email's domain has valid MX (Mail Exchange) records, indicating that it is capable of receiving emails.
-        domain = v.split("@")[1]
-        try:
-            dns.resolver.resolve(domain, "MX")
-        except dns.resolver.NoAnswer:
-            raise ValueError("Email domain has no MX records")
-
-        # TODO: Email verification logic should be handled by a separate service
-
-        return v.lower()
-
-
-class Owner(OwnerBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    # dashboard_data: Optional[DashboardData] = None
-    # facilities: List["Facility"] = Relationship(back_populates="owner")
-    managers: List["Manager"] = Relationship(back_populates="owner")
-
-    # def calculate_roi(self, investment: float, current_value: float) -> float:
-    #     if investment <= 0:
-    #         raise ValueError("Investment must be greater than zero")
-    #     roi = (current_value - investment) / investment
-    #     return round(roi, 2)
-
-    # def analyze_market_trends(self, market_data: dict) -> dict:
-    #     # Placeholder for market trend analysis logic
-    #     return market_data
-
-    # def communicate_with_gym_managers(self, message: str, mode: str = "email") -> str:
-    #     # Enhanced communication logic to include different modes (email, SMS, etc.)
-    #     return f"Message sent to gym managers via {mode}: {message}"
-
-
-class OwnerCreate(OwnerBase):
-    pass
-
-
-class OwnerRead(OwnerBase):
-    id: int
-
-
-class OwnerUpdate(SQLModel):
-    id: Optional[int] = None
-    name: Optional[str] = None
-    email: Optional[str] = None
-    role: Optional[Role] = None
-
-
-class ManagerBase(SQLModel):
-    name: constr(min_length=3, max_length=100)
-    email: str
-    role: Optional[Role] = None
-    created_at: Optional[datetime] = Field(default_factory=datetime.now)
-
-    owner_id: Optional[int] = Field(default=None, foreign_key="owner.id")
-
-    @field_validator("name")
-    def validate_name(cls, v):
-        if not v.replace(" ", "").isalpha():
-            raise ValueError("Name must only contain alphabetic characters")
-        return v.title()
-
-    @field_validator("email")
-    def validate_email(cls, v):
-        # Verify's that the email's domain has valid MX (Mail Exchange) records, indicating that it is capable of receiving emails.
-        domain = v.split("@")[1]
-        try:
-            dns.resolver.resolve(domain, "MX")
-        except dns.resolver.NoAnswer:
-            raise ValueError("Email domain has no MX records")
-
-        # TODO: Email verification logic should be handled by a separate service
-
-        return v.lower()
-
-
-class Manager(ManagerBase, table=True):
-    id: Optional[int] = Field(default=None, primary_key=True)
-    # financial_data: Optional[FinancialData] = None
-    # facilities: List["Facility"] = Relationship(back_populates="manager")
-    # staff: List["Staff"] = Relationship(back_populates="manager")
-    # trianers: List["Trainer"] = Relationship(back_populates="manager")
-
-    owner: Optional[Owner] = Relationship(back_populates="managers")
-
-    # def manage_staff(self, staff_id: int, action: str) -> str:
-    #     # Placeholder for staff management logic
-    #     return f"Action {action} executed for staff ID {staff_id}"
-
-    # def handle_finances(self, action: str, amount: float) -> str:
-    #     # Placeholder for financial handling logic
-    #     return f"Financial action {action} with amount {amount} executed"
-
-    # def organize_community_event(self, event_details: dict) -> str:
-    #     # Placeholder for event organization logic
-    #     return f"Community event organized with details: {event_details}"
-
-
-class ManagerRead(ManagerBase):
-    id: int
-
-
-class ManagerCreate(ManagerBase):
-    pass
-
-
-class ManagerUpdate(SQLModel):
-    id: Optional[int] = None
-    name: Optional[str] = None
-    email: Optional[str] = None
-    role: Optional[str] = None
-    owner_id: Optional[int] = None
-
-
-class ManagerReadWithOwner(ManagerRead):
-    owner: Optional[OwnerRead] = None
-
-
-class OwnerReadWithManagers(OwnerRead):
-    managers: List[ManagerRead] = []
-
-
-# DB_FILE = "gym_dot_infra_dev_db.sqlite3"
-# connect_args = {"check_same_thread": False}
-# engine = create_engine(f"sqlite:///{DB_FILE}", echo=True, connect_args=connect_args)
-
-
-# def create_tables():
-#     """Create the tables registered with SQLModel.metadata (i.e classes with table=True).
-#     More info: https://sqlmodel.tiangolo.com/tutorial/create-db-and-table/#sqlmodel-metadata
-#     """
-#     SQLModel.metadata.create_all(engine)
-from .database import engine, create_tables
+from .database import create_tables, engine
+from .models import (
+    Manager,
+    ManagerCreate,
+    ManagerRead,
+    ManagerReadWithOwner,
+    ManagerUpdate,
+    Owner,
+    OwnerCreate,
+    OwnerRead,
+    OwnerReadWithManagers,
+    OwnerUpdate,
+    FacilityRead,
+    FacilityReadWithOwner,
+    FacilityReadWithManager,
+    FacilityReadWithStaffAndTrainers,
+    FacilityUpdate,
+    FacilityCreate,
+    Facility,
+    TrainerRead,
+    TrainerReadWithOwner,
+    TrainerReadWithManager,
+    TrainerReadWithFacility,
+    TrainerUpdate,
+    TrainerCreate,
+    Trainer,
+    StaffRead,
+    StaffReadWithOwner,
+    StaffReadWithManager,
+    StaffReadWithFacility,
+    StaffUpdate,
+    StaffCreate,
+    Staff,
+)
 
 
 @asynccontextmanager
@@ -291,7 +76,7 @@ def get_manager(*, session: Session = Depends(get_session), manager_id: int):
 
 
 @app.get("/managers/", response_model=List[ManagerRead])
-def read_managers(
+def get_managers(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
@@ -348,7 +133,7 @@ def get_owner(*, session: Session = Depends(get_session), owner_id: int):
 
 
 @app.get("/owners/", response_model=List[OwnerRead])
-def read_owners(
+def get_owners(
     *,
     session: Session = Depends(get_session),
     offset: int = 0,
@@ -385,3 +170,235 @@ def delete_owner(*, session: Session = Depends(get_session), owner_id: int):
     session.delete(owner)
     session.commit()
     return owner
+
+
+@app.post("/facilities/", response_model=FacilityRead)
+def create_facility(
+    *, session: Session = Depends(get_session), facility: FacilityCreate
+):
+    db_facility = Facility.model_validate(facility)
+    session.add(db_facility)
+    session.commit()
+    session.refresh(db_facility)
+    return db_facility
+
+
+@app.get("/facilities/", response_model=List[FacilityRead])
+def get_facilities(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    facilities = session.exec(select(Facility).offset(offset).limit(limit)).all()
+    return facilities
+
+
+@app.get("/facilities/{facility_id}/owner/", response_model=FacilityReadWithOwner)
+def get_facility(*, session: Session = Depends(get_session), facility_id: int):
+    facility = session.get(Facility, facility_id)
+    if not facility:
+        raise HTTPException(status_code=404, detail="Facility not found")
+    return facility
+
+
+@app.get("/facilities/{facility_id}/manager/", response_model=FacilityReadWithManager)
+def get_facility_with_manager(
+    *, session: Session = Depends(get_session), facility_id: int
+):
+    facility = session.get(Facility, facility_id)
+    if not facility:
+        raise HTTPException(status_code=404, detail="Facility not found")
+    return facility
+
+
+@app.get(
+    "/facilities/{facility_id}/staff/trainers/",
+    response_model=FacilityReadWithStaffAndTrainers,
+)
+def get_facility_with_staff_and_trainers(
+    *, session: Session = Depends(get_session), facility_id: int
+):
+    facility = session.get(Facility, facility_id)
+    if not facility:
+        raise HTTPException(status_code=404, detail="Facility not found")
+    return facility
+
+
+@app.patch("/facilities/{facility_id}", response_model=FacilityRead)
+def update_facility(
+    *,
+    session: Session = Depends(get_session),
+    facility_id: int,
+    facility_update: FacilityUpdate,
+):
+    facility = session.get(Facility, facility_id)
+    if not facility:
+        raise HTTPException(status_code=404, detail="Facility not found")
+    facility_data = facility_update.dict(exclude_unset=True)
+    for key, value in facility_data.items():
+        setattr(facility, key, value)
+    session.add(facility)
+    session.commit()
+    session.refresh(facility)
+    return facility
+
+
+@app.delete("/facilities/{facility_id}", response_model=FacilityRead)
+def delete_facility(*, session: Session = Depends(get_session), facility_id: int):
+    facility = session.get(Facility, facility_id)
+    if not facility:
+        raise HTTPException(status_code=404, detail="Facility not found")
+    session.delete(facility)
+    session.commit()
+    return facility
+
+
+@app.post("/trainers/", response_model=TrainerRead)
+def create_trainer(*, session: Session = Depends(get_session), trainer: TrainerCreate):
+    db_trainer = Trainer.model_validate(trainer)
+    session.add(db_trainer)
+    session.commit()
+    session.refresh(db_trainer)
+    return db_trainer
+
+
+@app.get("/trainers/", response_model=List[TrainerRead])
+def get_trainers(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    trainers = session.exec(select(Trainer).offset(offset).limit(limit)).all()
+    return trainers
+
+
+@app.get("/trainers/{trainer_id}/owner/", response_model=TrainerReadWithOwner)
+def get_trainer_with_owner(*, session: Session = Depends(get_session), trainer_id: int):
+    trainer = session.get(Trainer, trainer_id)
+    if not trainer:
+        raise HTTPException(status_code=404, detail="Trainer not found")
+    return trainer
+
+
+@app.get("/trainers/{trainer_id}/manager/", response_model=TrainerReadWithManager)
+def get_trainer_with_manager(
+    *, session: Session = Depends(get_session), trainer_id: int
+):
+    trainer = session.get(Trainer, trainer_id)
+    if not trainer:
+        raise HTTPException(status_code=404, detail="Trainer not found")
+    return trainer
+
+
+@app.get("/trainers/{trainer_id}/facility/", response_model=TrainerReadWithFacility)
+def get_trainer_with_facility(
+    *, session: Session = Depends(get_session), trainer_id: int
+):
+    trainer = session.get(Trainer, trainer_id)
+    if not trainer:
+        raise HTTPException(status_code=404, detail="Trainer not found")
+    return trainer
+
+
+@app.patch("/trainers/{trainer_id}", response_model=TrainerRead)
+def update_trainer(
+    *,
+    session: Session = Depends(get_session),
+    trainer_id: int,
+    trainer_update: TrainerUpdate,
+):
+    trainer = session.get(Trainer, trainer_id)
+    if not trainer:
+        raise HTTPException(status_code=404, detail="Trainer not found")
+    trainer_data = trainer_update.dict(exclude_unset=True)
+    for key, value in trainer_data.items():
+        setattr(trainer, key, value)
+    session.add(trainer)
+    session.commit()
+    session.refresh(trainer)
+    return trainer
+
+
+@app.delete("/trainers/{trainer_id}", response_model=TrainerRead)
+def delete_trainer(*, session: Session = Depends(get_session), trainer_id: int):
+    trainer = session.get(Trainer, trainer_id)
+    if not trainer:
+        raise HTTPException(status_code=404, detail="Trainer not found")
+    session.delete(trainer)
+    session.commit()
+    return trainer
+
+
+@app.post("/staff/", response_model=StaffRead)
+def create_staff(*, session: Session = Depends(get_session), staff: StaffCreate):
+    db_staff = Staff.model_validate(staff)
+    session.add(db_staff)
+    session.commit()
+    session.refresh(db_staff)
+    return db_staff
+
+
+@app.get("/staff/", response_model=List[StaffRead])
+def read_staff(
+    *,
+    session: Session = Depends(get_session),
+    offset: int = 0,
+    limit: int = Query(default=100, le=100),
+):
+    staff = session.exec(select(Staff).offset(offset).limit(limit)).all()
+    return staff
+
+
+@app.get("/staff/{staff_id}/owner/", response_model=StaffReadWithOwner)
+def get_staff_with_owner(*, session: Session = Depends(get_session), staff_id: int):
+    staff = session.get(Staff, staff_id)
+    if not staff:
+        raise HTTPException(status_code=404, detail="Staff not found")
+    return staff
+
+
+@app.get("/staff/{staff_id}/manager/", response_model=StaffReadWithManager)
+def get_staff_with_manager(*, session: Session = Depends(get_session), staff_id: int):
+    staff = session.get(Staff, staff_id)
+    if not staff:
+        raise HTTPException(status_code=404, detail="Staff not found")
+    return staff
+
+
+@app.get("/staff/{staff_id}/facility/", response_model=StaffReadWithFacility)
+def get_staff_with_facility(*, session: Session = Depends(get_session), staff_id: int):
+    staff = session.get(Staff, staff_id)
+    if not staff:
+        raise HTTPException(status_code=404, detail="Staff not found")
+    return staff
+
+
+@app.patch("/staff/{staff_id}", response_model=StaffRead)
+def update_staff(
+    *,
+    session: Session = Depends(get_session),
+    staff_id: int,
+    staff_update: StaffUpdate,
+):
+    staff = session.get(Staff, staff_id)
+    if not staff:
+        raise HTTPException(status_code=404, detail="Staff not found")
+    staff_data = staff_update.dict(exclude_unset=True)
+    for key, value in staff_data.items():
+        setattr(staff, key, value)
+    session.add(staff)
+    session.commit()
+    session.refresh(staff)
+    return staff
+
+
+@app.delete("/staff/{staff_id}", response_model=StaffRead)
+def delete_staff(*, session: Session = Depends(get_session), staff_id: int):
+    staff = session.get(Staff, staff_id)
+    if not staff:
+        raise HTTPException(status_code=404, detail="Staff not found")
+    session.delete(staff)
+    session.commit()
+    return staff
